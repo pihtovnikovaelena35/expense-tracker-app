cdda51b4d29cfaba2fba005c9423f4f31d32a44a
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2022-02-24 09:38:48 +0100

    Added a Chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import React, {useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
 
-  const expenses = [
+const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -32,9 +31,14 @@ const App = () => {
   ]
 
 
+
+const App = () => {
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+   setExpenses((prevExpenses)=> {
+     return[expense, ...prevExpenses];
+   });
   }
 
 
